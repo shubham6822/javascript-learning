c0ebf889393d5e11f78c4553873e1bb0ad76837e
Shubham <122037256+shubham6822@users.noreply.github.com>
2024-05-01 10:00:38 +0000

    variables

---
+++ basic/01Variables.js
@@ -0,0 +1,10 @@
+const acId = "12345"
+let acName = "shubham"
+var acPassword = "89858"
+acCity = "surat"
+
+//acId = "89685" //not allowed
+
+console.log(acId);
+
+console.table([acId,acName,acPassword,acCity])
