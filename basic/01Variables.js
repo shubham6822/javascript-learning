639275586583e81429ba7bf3b6177fd97a913adb
Shubham <122037256+shubham6822@users.noreply.github.com>
2024-05-01 10:06:03 +0000

    #

--- basic/01Variables.js
+++ basic/01Variables.js
@@ -2,9 +2,10 @@ const acId = "12345"
 let acName = "shubham"
 var acPassword = "89858"
 acCity = "surat"
+let acState;
 
 //acId = "89685" //not allowed
 
 console.log(acId);
 
-console.table([acId,acName,acPassword,acCity])
+console.table([acId,acName,acPassword,acCity,acState])
