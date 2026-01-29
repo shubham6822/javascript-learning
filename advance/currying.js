e9f7b535584f5908e48b892f2ecc81d72414894c
Shubham <sharmashubham6822@gmail.com>
2026-01-29 10:49:45 +0530

    currying

--- advance/currying.js
+++ advance/currying.js
@@ -1,3 +1,4 @@
+// Currying example using bind
 let multiple = (a, b) => {
   return a * b;
 };
@@ -8,3 +9,14 @@ console.log(multiple(2, 3)); // Output: 6
 let multipleByTwo = multiple.bind(this, 2);
 
 console.log(multipleByTwo(5)); // Output: 10
+
+// Currying example using closures
+let multiply = (a) => {
+  return (b) => {
+    return a * b;
+  };
+};
+
+let multiplyByThree = multiply(3);
+
+console.log(multiplyByThree(4)); // Output: 12
