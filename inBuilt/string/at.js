93dfd158a8f401d0435d780f9c3eac6bfd0d91eb
Shubham <sharmashubham6822@gmail.com>
2026-02-04 13:00:01 +0530

    string at method

--- inBuilt/string/at.js
+++ inBuilt/string/at.js
@@ -6,16 +6,22 @@ function customAt(text, index) {
       if (i === index) return text[i];
     }
   } else {
-    for (i = text.length; i > 0; i--) {
-      console.log("i", i, text[i]);
-      if (Math.abs(index) == i) {
-        return text[i];
-      }
+    let rightIndex = text.length - Math.abs(index);
+    for (i = 0; i < text.length; i++) {
+      if (i === rightIndex) return text[i];
     }
   }
+}
+
+function customOptimizationAt(text, index) {
+  // Convert negative index to positive
+  const actualIndex = index < 0 ? text.length + index : index;
+
+  return text[actualIndex];
 }
 
 // Testing the custom charAt method
 console.log("String:", text);
 console.log("Custom charAt(7):", customAt(text, -2));
+console.log("customOptimizationAt", customOptimizationAt(text, -2));
 console.log("Built-in charAt(7):", text.at(-2));
